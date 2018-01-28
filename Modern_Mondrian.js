12306ad69edcbe659c74ad778c54214c5898d6d3
mitchwong2021 <34347774+mitchwong2021@users.noreply.github.com>
2018-01-27 22:43:47 -0800

    Create Modern_Mondrian.js

---
+++ Modern_Mondrian.js
@@ -0,0 +1,12 @@
+while(true) {		//draws random rectangles, with random colors, with strokes around them
+  fill(random(255), random(255), random(255));
+  rect(random(400), random(400), random(200), random(200));
+}	
+
+//something different below...
+
+/*while(true) {		
+  background(random(255), random(255), random(255));
+  fill(random(255), random(255), random(255));
+  rect(random(400), random(400), random(200), random(200), 200, 200);
+}  */
