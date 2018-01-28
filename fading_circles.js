5bc38f8ade187b6294a2eb8d9394c9dce471994d
mitchwong2021 <34347774+mitchwong2021@users.noreply.github.com>
2018-01-27 22:05:27 -0800

    Create fading_circles.js

---
+++ fading_circles.js
@@ -0,0 +1,11 @@
+background(255);
+colorMode(HSB);
+noStroke();
+
+while(true) {
+  background(0, 0, 255, 0.1);
+  fill(200, 50, 200);
+  ellipse(random(width), random(height), random(1, 21));
+}
+
+//those are my tears
